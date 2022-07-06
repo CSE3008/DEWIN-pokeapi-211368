fee9107280a8ca3c3b6508f19dd8d66c792df254
CSE3008 <211368@utags.edu.mx>
2022-07-05 21:12:36 -0500

    feat: add routes and new .jsx to work with context and router

---
+++ src/Routes/allpokemons.jsx
@@ -0,0 +1,8 @@
+export default function allpokemons (){
+    return(
+        <main style= {{padding: "1rem 0"}}>
+         <h1>All Pokemons</h1>   
+        </main>
+    );
+
+}
---
+++ src/Routes/pokesearch.jsx
@@ -0,0 +1,8 @@
+export default function pokesearch (){
+    return(
+        <main style= {{padding: "lrem 0"}}>
+            <h1>Pokesearch</h1>
+        </main>
+    );
+
+}

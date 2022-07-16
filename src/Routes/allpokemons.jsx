ecd8f1a4eb7b852fa309cbe355eb4b81f7e2b53e
CSE3008 <211368@utags.edu.mx>
2022-07-16 02:14:31 -0500

    style: change pokemon name from card

--- src/Routes/allpokemons.jsx
+++ src/Routes/allpokemons.jsx
@@ -68,8 +68,10 @@ export default function Allpokemons() {
         
       
         <div className="container">
-          
+          <div className="row">
           <Pokecard data={pokeinfo}/>
+          </div>
+          
           
           <div className="row row-cols-1 row-cols-md-3 g-4">
             {pagepokemons().map((pokemon) => (
--- src/Routes/routeHija/pokecard.jsx
+++ src/Routes/routeHija/pokecard.jsx
@@ -10,7 +10,7 @@ const Pokecard=({data})=>{
 
                 
                 <main>
-                <h3>{data.name}</h3>
+                <h2>{data.name}</h2>
                 <img src={data.sprites.other.dream_world.front_default}></img>
                 <div className="abilities">
                     {data.abilities.map(poke=>{return(
@@ -22,8 +22,7 @@ const Pokecard=({data})=>{
                 <div>
                 {data.stats.map(poke=>{return(
                  <div>
-                    <h6><strong>{poke.stat.name}:</strong> {poke.base_stat}</h6></div>        
-                        
+                    <h6><strong>{poke.stat.name}:</strong> {poke.base_stat}</h6></div>                            
                         
                         
                     )})}
